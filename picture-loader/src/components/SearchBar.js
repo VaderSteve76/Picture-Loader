0b00924c049b4d260fe790e89e6ff0f2a2cdaf87
VaderSteve76 <alverson.s@yahoo.com>
2021-08-18 15:23:49 -0400

    Added input and form change function

--- picture-loader/src/components/SearchBar.js
+++ picture-loader/src/components/SearchBar.js
@@ -2,17 +2,24 @@ import { React, Component } from 'react';
 
 class SearchBar extends Component {
 
-  onInputChange(e) {
-    
+  state = { term: '' };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term)
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={(e) => this.onFormSubmit(e)}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={(e) => this.setState({ term: e.target.value })} 
+            />
           </div>
         </form>
       </div>
