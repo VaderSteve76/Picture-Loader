5da2998888e97813dcf1d3ebb669484105b7f4c2
VaderSteve76 <alverson.s@yahoo.com>
2021-08-18 14:03:18 -0400

    Added semantic ui

--- picture-loader/src/components/SearchBar.js
+++ picture-loader/src/components/SearchBar.js
@@ -1,11 +1,19 @@
 import { React, Component } from 'react';
 
 class SearchBar extends Component {
+
+  onInputChange(e) {
+    
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" placeholder="Search" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange} />
+          </div>
         </form>
       </div>
     )
