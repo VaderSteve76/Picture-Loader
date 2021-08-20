5c2592d763a2952e854ad08671bdf811fa900e9e
VaderSteve76 <alverson.s@yahoo.com>
2021-08-20 14:38:47 -0400

    Added async,await and getting images

--- picture-loader/src/components/App.js
+++ picture-loader/src/components/App.js
@@ -4,21 +4,26 @@ import SearchBar from './SearchBar';
 
 class App extends Component {
 
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: {
-        query: term
-      },
-      headers: {
-        Authorization: 'Client-ID Z_bQJn2PjhqdUe8uJm2c_C-n7LMdOruT8JWk2-Ip058'
-      }
-    });
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const res = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization: 'Client-ID Z_bQJn2PjhqdUe8uJm2c_C-n7LMdOruT8JWk2-Ip058'
+        }
+      });
+      this.setState({ images: res.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '20px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
