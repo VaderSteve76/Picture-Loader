24eb7604b93e6bbd3cdcc3fc344e76fd8b89ea2b
VaderSteve76 <alverson.s@yahoo.com>
2021-08-24 12:10:10 -0400

    Refactored to React.Component, also added createRef

--- picture-loader/src/components/ImageCard.js
+++ picture-loader/src/components/ImageCard.js
@@ -1,11 +1,23 @@
-import { React, Component } from 'react';
+import React from 'react';
 
-class ImageCard extends Component {
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
     const { description, urls } = this.props.image;
+
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
